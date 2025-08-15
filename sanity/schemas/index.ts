9c9036d0bc0c2fc63bff1f4df37fb26c9493ea58
vanloc <tovanloc19@gmail.com>
2025-08-15 22:58:43 +0700

    setup sanity

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,9 +1,11 @@
 import { postSchema } from './post-schema';
 import { categorySchema } from './category-schema';
+import { bannerSchema } from './banner-schema';
 
 const schemas = [
   postSchema,
   categorySchema,
+  bannerSchema,
 ];
 
 export default schemas;
