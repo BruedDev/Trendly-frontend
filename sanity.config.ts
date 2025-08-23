d950781efda3141081583dc59d78d9cd482acfe0
vanloc <tovanloc19@gmail.com>
2025-08-23 14:55:19 +0700

    edit sanity.config

--- sanity.config.ts
+++ sanity.config.ts
@@ -9,8 +9,6 @@ import { table } from "@sanity/table";
 export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || '',
-  // projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'a9ulze55',
-  // dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   title: "Your Project Title",
   apiVersion: "2024-11-01",
   basePath: "/admin/sanity",
