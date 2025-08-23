c1734c22b5eccf4d734074eb8059dc98e715558e
vanloc <tovanloc19@gmail.com>
2025-08-23 15:53:33 +0700

    fix test add a link navigationLink

--- src/components/NavigationLink/index.tsx
+++ src/components/NavigationLink/index.tsx
@@ -12,17 +12,18 @@ const NavigationLink = React.forwardRef<HTMLAnchorElement, NavigationLinkProps>(
     };
 
     return (
-      <Link
-        href={href}
-        className={`${styles["nav-link"]} ${className} ${
-          active ? styles["nav-active"] : ""
-        }`.trim()}
-        ref={ref}
-        onClick={handleClick}
-      >
-        <span className={styles["nav-text"]} data-text={children}>
-          {children}
-        </span>
+      <Link href={href} passHref legacyBehavior>
+        <a
+          ref={ref}
+          className={`${styles["nav-link"]} ${className} ${
+            active ? styles["nav-active"] : ""
+          }`.trim()}
+          onClick={handleClick}
+        >
+          <span className={styles["nav-text"]} data-text={children}>
+            {children}
+          </span>
+        </a>
       </Link>
     );
   }
