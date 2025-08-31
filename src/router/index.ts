17ad321ad5c543570895973486a97d60d44d8b5a
vanloc <tovanloc19@gmail.com>
2025-08-31 20:02:19 +0700

    import AUTH_ROUTES

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import THEME_ROUTES from "./Theme-routes";
 import CART_ROUTES from "./Cart-routes";
+import AUTH_ROUTES from "./auth-routes"
 
 const API_ROUTES = {
   theme: THEME_ROUTES,
   cart: CART_ROUTES,
+  auth: AUTH_ROUTES,
 };
 
 export default API_ROUTES;
