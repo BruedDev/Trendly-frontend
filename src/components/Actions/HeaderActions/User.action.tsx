4bf5e72bf5ccac3f647009d8026eff86a4ee78fc
vanloc <tovanloc19@gmail.com>
2025-08-31 20:00:55 +0700

    demo to test token and user

--- src/components/Actions/HeaderActions/User.action.tsx
+++ src/components/Actions/HeaderActions/User.action.tsx
@@ -1,10 +1,17 @@
 import Tooltip from "@/ui/Tooltip";
 import { CiUser } from "react-icons/ci";
+import { useRouter } from "next/navigation";
 
 export default function UserComponent() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/account");
+  };
+
   return (
-    <Tooltip title="Đăng nhập" arrow="top-center">
-      <button data-original-title="Đăng nhập">
+    <Tooltip title="Tài khoản" arrow="top-center">
+      <button data-original-title="Tài khoản" onClick={handleClick}>
         <CiUser />
       </button>
     </Tooltip>
