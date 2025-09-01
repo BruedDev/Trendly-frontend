a7046e7fe93ae9fa97f283a54815a0fb182d753b
vanloc <tovanloc19@gmail.com>
2025-09-02 03:34:48 +0700

    Add Preview component and refactor ViewCart; replace old Preview with new implementation in ActionsProduct and CartFooter

--- src/components/Actions/cart/Preview.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface PreviewProps {
-  className?: string;
-}
-
-export default function Preview({ className }: PreviewProps) {
-  return <button className={className}>Xem nhanh</button>;
-}
