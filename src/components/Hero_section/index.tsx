38b37360db8c3e7c77392da897f229fa117546bf
vanloc <tovanloc19@gmail.com>
2025-09-05 19:07:18 +0700

    update

--- src/components/Hero_section/index.tsx
+++ src/components/Hero_section/index.tsx
@@ -7,33 +7,44 @@ import styles from "./HeroSection.module.scss";
 import { BannerSection } from "@/types/Hero_section";
 
 export default function HeroSection({ data }: { data?: BannerSection }) {
-  if (!data || !data.images || data.images.length === 0) return null;
-
-  const images = data.images.filter((img) => img?.asset?.url);
+  const images = data?.images?.filter((img) => img?.asset?.url) || [];
 
   return (
     <div className={styles.container}>
-      <SwiperSlide
-        data={images}
-        renderItem={(image, index) => (
-          <div className={styles.imageWrapper}>
-            <Image
-              src={image.asset!.url!}
-              alt={image.alt || `slide ${index + 1}`}
-              width={1000}
-              height={1000}
-              priority={index === 0}
-              className={styles.image}
-            />
-          </div>
-        )}
-        swiperProps={{
-          slidesPerView: 1,
-          loop: true,
-          autoplay: { delay: 5000 },
-        }}
-        transitionSpeed={getTimeSpeed(1000)}
-      />
+      {images.length === 0 ? (
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/images/hero.avif"
+            alt="Hero static"
+            width={1000}
+            height={1000}
+            priority
+            className={styles.image}
+          />
+        </div>
+      ) : (
+        <SwiperSlide
+          data={images}
+          renderItem={(image, index) => (
+            <div className={styles.imageWrapper}>
+              <Image
+                src={image.asset!.url!}
+                alt={image.alt || `slide ${index + 1}`}
+                width={1000}
+                height={1000}
+                priority={index === 0}
+                className={styles.image}
+              />
+            </div>
+          )}
+          swiperProps={{
+            slidesPerView: 1,
+            loop: true,
+            autoplay: { delay: 5000 },
+          }}
+          transitionSpeed={getTimeSpeed(800)}
+        />
+      )}
     </div>
   );
 }
