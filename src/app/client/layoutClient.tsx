e72cce62f314b819de4cb02ca99ba2c0d72e4c0a
vanloc <tovanloc19@gmail.com>
2025-09-12 12:41:57 +0700

    feat: Refactor layout to include Providers component for context management

--- src/app/client/layoutClient.tsx
+++ src/app/client/layoutClient.tsx
@@ -3,44 +3,24 @@
 import { useEffect } from "react";
 import Header from "@/components/Header";
 import Overlay from "@/components/Overlay";
-import ContextProvider from "@/contexts/index";
-import { StatusMessageProvider } from "@/contexts/StatusMessageContext";
 import { useOverlay } from "@/hooks/useOverlay";
 import StatusMessage from "@/components/StatusMessage";
 import FlyToCart from "@/components/FlyToCart";
 import { useFlyToCart } from "@/hooks/useFlyToCart";
-
-import SuspenseWrapper from "@/components/Suspense";
-import { Provider } from "react-redux";
-import store from "@/store";
 
 export default function LayoutClient({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { isOpen, closeOverlay, isExiting } = useOverlay();
+  const { flyToCartData, completeFlyToCart } = useFlyToCart();
+
   useEffect(() => {
     requestAnimationFrame(() => {
       setTimeout(() => {}, 200);
     });
   }, []);
-
-  return (
-    <Provider store={store}>
-      <StatusMessageProvider>
-        <ContextProvider>
-          <SuspenseWrapper>
-            <LayoutContent>{children}</LayoutContent>
-          </SuspenseWrapper>
-        </ContextProvider>
-      </StatusMessageProvider>
-    </Provider>
-  );
-}
-
-function LayoutContent({ children }: { children: React.ReactNode }) {
-  const { isOpen, closeOverlay, isExiting } = useOverlay();
-  const { flyToCartData, completeFlyToCart } = useFlyToCart();
 
   const createStartElementId = () => {
     if (!flyToCartData.productId) return "";
